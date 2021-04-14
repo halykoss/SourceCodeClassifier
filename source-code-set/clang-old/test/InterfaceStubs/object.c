3561a0e53cc316b3efb91af6922bbdd7354c9812
Federico <federico.p98@gmail.com>
2021-04-14 19:20:55 +0200

    Check if smodules has been fixed

--- source-code-set/clang-old/test/InterfaceStubs/object.c
+++
@@ -1,7 +0,0 @@
-// RUN: %clang -fvisibility=default -c -o - -emit-interface-stubs %s | FileCheck -check-prefix=CHECK-TAPI %s
-// RUN: %clang -fvisibility=default -c -o - -emit-interface-stubs %s | FileCheck -check-prefix=CHECK-SYMBOLS %s
-// RUN: %clang -fvisibility=default -c -o - %s | llvm-nm - 2>&1 | FileCheck -check-prefix=CHECK-SYMBOLS %s
-
-// CHECK-TAPI: data" : { Type: Object, Size: 4 }
-// CHECK-SYMBOLS: data
-int data = 42;
