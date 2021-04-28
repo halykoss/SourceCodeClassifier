b0f97128a1097560b86865a1412cfff19b17e704
Federico <federico.p98@gmail.com>
2021-04-28 19:02:18 +0200

    Update submodules and Linguist

--- linguist_indexer.rb
+++ linguist_indexer.rb
@@ -12,14 +12,26 @@ end
 
 num_split = 10
 
+#puts "Recuperati" + pathes.size.to_s
+
 splitted_arr = pathes.each_slice( (pathes.size/num_split).round ).to_a
+
+#puts "Divisi in " + splitted_arr.size.to_s  + " array "
+
+#puts "da"
+#splitted_arr.each do |obj|
+#    puts obj.size.to_s + " elementi"
+#end
 
 pool = Thread.pool(num_split)
 
 splitted_arr.each do |obj|
     pool.process do
         obj.each do |value|
-            puts value + "," + Linguist::FileBlob.new(value).language.name + "\n"
+            begin
+                puts value + "," + Linguist::FileBlob.new(value).language.name + "\n"
+            rescue
+            end
         end
     end
 end
